93f4abf45d9881c4e3b68c0b411d71280e0293d5
Hugo Schlecken <h.schlecken@gmx.de>
2010-09-15 01:02:20 +0200

    first running unittest using gmp framework

--- src-test/gmp-unittest-base.h
+++ src-test/gmp-unittest-base.h
@@ -9,7 +9,7 @@
 #import <SenTestingKit/SenTestingKit.h>
 
 
-@interface first : SenTestCase {
+@interface GmpUnittestBase : SenTestCase {
 
 }
 
--- src-test/gmp-unittest-base.m
+++ src-test/gmp-unittest-base.m
@@ -13,10 +13,10 @@
 #import "longlong.h"
 #import "tests.h"
 
-#import "first.h"
+#import "gmp-unittest-base.h"
 
 
-@implementation first
+@implementation GmpUnittestBase
 
 /**
  *
@@ -36,6 +36,9 @@
   tests_end ();
 }
 
+/**
+ *
+ */
 -(void) testCheckData
 {
 #define M  MP_LIMB_T_MAX
@@ -72,11 +75,15 @@
       mp_limb_trace ("  want dl", data[i].want_dl);
       mp_limb_trace ("  got dh ", got_dh);
       mp_limb_trace ("  got dl ", got_dl);
-      abort ();
+      
+      STFail(@"check_data wrong at data[%d].",i);
     }
   }
 }
 
+/**
+ *
+ */
 -(void) testCheckRandom
 {
   mp_limb_t  want_dh,want_dl, got_dh,got_dl, mh,ml, sh,sl;
@@ -104,9 +111,82 @@
       mp_limb_trace ("  want dl", want_dl);
       mp_limb_trace ("  got dh ", got_dh);
       mp_limb_trace ("  got dl ", got_dl);
-      abort ();
+    
+      STFail(@"check_data wrong at data[%d].",i);
     }
   }
+}
+
+/**
+ *
+ */
+-(void) testPopC
+{
+  mp_limb_t  src, want, got;
+  int        i;
+  
+  mp_trace_base = -16;
+  
+  for (i = 0; i < GMP_LIMB_BITS; i++)
+  {
+    src = CNST_LIMB(1) << i;
+    want = 1;
+    
+    popc_limb (got, src);
+    if (got != want)
+    {
+    error:
+      printf ("popc_limb wrong result\n");
+      mpn_trace ("  src ", &src,  (mp_size_t) 1);
+      mpn_trace ("  want", &want, (mp_size_t) 1);
+      mpn_trace ("  got ", &got,  (mp_size_t) 1);
+      
+      STFail(@"popc_limb wrong result.");
+    }
+  }
+    
+  for (i = 0; i < 100; i++)
+  {
+    mpn_random2 (&src, (mp_size_t) 1);
+    want = ref_popc_limb (src);
+    
+    popc_limb (got, src);
+    if (got != want)
+      goto error;
+  }
+  
+
+}
+
+
+/**
+ *
+ */
+-(void)checkOne :(int) want :(unsigned long)n
+{
+  int  got;
+  ULONG_PARITY (got, n);
+  if (got != want)
+  {
+    STFail(@"ULONG_PARITY wrong, n=%lX, want=%d, got=%d.",n,want,got);
+  }
+}
+
+/**
+ *
+ */
+-(void) testCheckVarious
+{
+  int  i;
+  
+  [self checkOne:0 :0L];
+  [self checkOne:BITS_PER_ULONG & 1 :ULONG_MAX];
+  [self checkOne:0 :0x11L];
+  [self checkOne:1 :0x111L];
+  [self checkOne:1 :0x3111L];
+  
+  for (i = 0; i < BITS_PER_ULONG; i++)
+    [self checkOne:1 :1L << i]  ;
 }
 
 
