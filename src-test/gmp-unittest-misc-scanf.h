41592e60e2094fc2409b96ca68b01f3168840b25
Hugo Schlecken <h.schlecken@gmx.de>
2010-11-03 02:11:09 +0100

    add unittest misc-scanf

---
+++ src-test/gmp-unittest-misc-scanf.h
@@ -0,0 +1,17 @@
+//
+//  gmp-unittest-misc-scanf.h
+//  gmp
+//
+//  Created by Feldmaus on 2.11.10.
+//  Copyright 2010 Stefan Thomas. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface gmp_unittest_misc_scanf : SenTestCase 
+{
+
+}
+
+@end
